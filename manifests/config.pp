18fd4b6baf310563288738dfbbcf83a6305438ee
Silke <silke@slxh.eu>
2016-04-13 20:34:15 +0200

    Config file should be owned by root:root

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,7 +6,7 @@ class unattended_upgrades::config inherits unattended_upgrades {
     path    => $::unattended_upgrades::params::auto_upgrades_f,
     content => epp('unattended_upgrades/auto_upgrades.epp'),
     owner   => root,
-    #group   => root,
+    group   => root,
     mode    => 'u=rw,go=r',
   }
 
@@ -15,7 +15,7 @@ class unattended_upgrades::config inherits unattended_upgrades {
     path    => $::unattended_upgrades::params::unattended_upgrades_f,
     content => epp('unattended_upgrades/unattended_upgrades.epp'),
     owner   => root,
-    #group   => root,
+    group   => root,
     mode    => 'u=rw,go=r',
   }
 
